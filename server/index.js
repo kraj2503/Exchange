2839a3e8ded5f3fa6d24b4f3f199b49f36436782
kraj2503 <73212876+kraj2503@users.noreply.github.com>
2025-03-17 01:39:41 +0530

    Init Api proxy server

---
+++ server/index.js
@@ -0,0 +1,54 @@
+
+
+const express = require("express")
+const axios = require("axios")
+require('dotenv').config()
+const app = express()
+
+
+const BASE_URL = process.env.BASE_URL
+console.log(BASE_URL)
+app.get('/v1/klines', async (req, res) => {
+        try {
+
+                const { symbol, interval, startTime, endTime } =  req.query
+                const result = await axios.get(`${BASE_URL}klines?symbol=${symbol}&interval=${interval}&startTime=${startTime}&endTime=${endTime}`);
+
+                console.log(result.data);
+                res.json(result.data);
+        }
+        catch {
+                res.json({ "Error": "error" });
+        }
+})
+
+app.get('/v1/markets',async(req,res)=>{
+
+        try{
+                const {symbol} =  req.query
+                const result = await axios.get(`${BASE_URL}markets`)
+                console.log(result.data)
+                res.json(result.data);
+        }
+        catch {
+                res.json({ "Error": "error" });
+        }
+})
+
+app.get('/v1/depth', async (req,res)=>{
+        
+
+        const {symbol} =  req.query
+        const result = await axios.get(`${BASE_URL}depth?symbol=${symbol}`)
+        console.log(result.data)
+        res.json(result.data);
+
+})
+
+
+
+app.use(function (err, req, res, next) {
+        res.status(500);
+        res.send("Oops, something went wrong.")
+    });
+app.listen(3005)
