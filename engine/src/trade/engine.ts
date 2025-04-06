c3015251ac5f68ac73b8b88984af778a41951c8a
kraj2503 <73212876+kraj2503@users.noreply.github.com>
2025-04-07 03:34:16 +0530

    switch process for order

--- engine/src/trade/engine.ts
+++ engine/src/trade/engine.ts
@@ -1,5 +1,12 @@
 import { RedisManager } from "../redisManager";
-import { CREATE_ORDER, MessageFromApi } from "../types/fromApi";
+import {
+  CANCEL_ORDER,
+  CREATE_ORDER,
+  GET_DEPTH,
+  GET_OPEN_ORDERS,
+  MessageFromApi,
+} from "../types/fromApi";
+import { ON_RAMP } from "../types/toApi";
 import { Order, OrderBook } from "./orderBook";
 import { readFileSync, writeFileSync } from "node:fs";
 
@@ -76,6 +83,108 @@ export class Engine {
               orderId: "",
               executedQty: 0,
               remainingQty: 0,
+            },
+          });
+        }
+        break;
+      case CANCEL_ORDER:
+        try {
+          const orderId = message.data.orderId;
+          const cancelMarket = message.data.market;
+          const cancelOrderbook = this.orderBook.find(
+            (o) => o.ticker() === cancelMarket
+          );
+          const quoteAsset = cancelMarket.split("_")[1];
+          if (!cancelOrderbook) {
+            throw new Error("No OrderBook Found");
+          }
+
+          const order =
+            cancelOrderbook.asks.find((o) => o.orderId === orderId) ||
+            cancelOrderbook.bids.find((o) => o.orderId === orderId);
+          if (!order) {
+            console.log("No Order Found");
+            throw new Error("No order Found");
+          }
+
+          if (order.side === "buy") {
+            const price = cancelOrderbook.cancelBid(order);
+            const leftQuantity = (order.quantity - order.filled) * order.price;
+            //@ts-ignore
+            this.balances.get(order.userId)[BASE_CURRENCY].available +=
+              leftQuantity;
+            //@ts-ignore
+            this.balances.get(order.userId)[BASE_CURRENCY].locked -=
+              leftQuantity;
+            if (price) {
+              this.sendUpdatedDepthAt(price.toString(), cancelMarket);
+            }
+          } else {
+            const price = cancelOrderbook.cancelAsk(order);
+            const leftQuantity = order.quantity - order.filled;
+            //@ts-ignore
+            this.balances.get(order.userId)[quoteAsset].available +=
+              leftQuantity;
+            //@ts-ignore
+            this.balances.get(order.userId)[quoteAsset].locked -= leftQuantity;
+            if (price) {
+              this.sendUpdatedDepthAt(price.toString(), cancelMarket);
+            }
+          }
+          RedisManager.getInstance().sendToApi(clientId, {
+            type: "ORDER_CANCELLED",
+            payload: {
+              orderId,
+              executedQty: 0,
+              remainingQty: 0,
+            },
+          });
+        } catch (e) {
+          console.log("Error hwile cancelling order");
+          console.log(e);
+        }
+        break;
+      case GET_OPEN_ORDERS:
+        try {
+          const openOrderbook = this.orderBook.find(
+            (o) => o.ticker() === message.data.market
+          );
+          if (!openOrderbook) {
+            throw new Error("No orderbook found");
+          }
+          const openOrders = openOrderbook.getOpenOrders(message.data.userId);
+
+          RedisManager.getInstance().sendToApi(clientId, {
+            type: "OPEN_ORDERS",
+            payload: openOrders,
+          });
+        } catch (e) {
+          console.log(e);
+        }
+        break;
+      case ON_RAMP:
+        const userId = message.data.userId;
+        const amount = Number(message.data.amount);
+        this.onRamp(userId, amount);
+        break;
+      case GET_DEPTH:
+        try {
+          const market = message.data.market;
+          const orderbook = this.orderBook.find((o) => o.ticker() === market);
+          if (!orderbook) {
+            throw new Error("No orderbook found");
+          }
+          RedisManager.getInstance().sendToApi(clientId, {
+            type: "DEPTH",
+            payload: orderbook.getDepth(),
+          });
+        } catch (e) {
+          console.log(e);
+          RedisManager.getInstance().sendToApi(clientId, {
+            type: "DEPTH",
+            payload: {
+              bids: [],
+              asks: [],
             },
           });
         }
