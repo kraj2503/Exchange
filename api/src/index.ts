3c36647ab7f41081c5abd9c731bb6fa478bf3f5d
kraj2503 <73212876+kraj2503@users.noreply.github.com>
2025-04-09 03:45:12 +0530

    apis

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,15 @@
 import express from "express";
-// import cors from "cors";
+import cors from "cors"
 import { depthRouter } from "./routes/depth";
+import { orderRouter } from "./routes/order";
 
 const app = express();
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/depth", depthRouter);
-
+app.use("/api/v1/order", orderRouter);
+app.use("/api/v1/depth", depthRouter);
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
