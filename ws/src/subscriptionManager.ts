9606cc2d2e6fa4a090ba17dd0f6eddeaa549ce68
kraj2503 <73212876+kraj2503@users.noreply.github.com>
2025-04-19 01:40:17 +0530

    Ädded unsubscribe

--- ws/src/subscriptionManager.ts
+++ ws/src/subscriptionManager.ts
@@ -1,5 +1,6 @@
 import { createClient, RedisClientType } from "redis";
 import { UserManager } from "./userManager";
+import { CLOSING } from "ws";
 
 export class SubscriptionManager {
   private static instance: SubscriptionManager;
@@ -18,33 +19,62 @@ export class SubscriptionManager {
     }
     return this.instance;
   }
-public subscribe(userId:string,subscription:string){
-
-    if(this.subscriptions.get(userId)?.includes(subscription)){
-        return 
+  public subscribe(userId: string, subscription: string) {
+    if (this.subscriptions.get(userId)?.includes(subscription)) {
+      return;
     }
 
-    this.subscriptions.set(userId,(this.subscriptions.get(userId)||[]).concat(subscription))
-    this.reverseSubscriptions.set(userId,(this.reverseSubscriptions.get(userId)||[]).concat(subscription))
-    
-    if(this.reverseSubscriptions.get(subscription)?.length===1){
-        this.redisClient.subscribe(subscription,this.redisCallbackHandler)
+    this.subscriptions.set(
+      userId,
+      (this.subscriptions.get(userId) || []).concat(subscription)
+    );
+    this.reverseSubscriptions.set(
+      userId,
+      (this.reverseSubscriptions.get(userId) || []).concat(subscription)
+    );
+
+    if (this.reverseSubscriptions.get(subscription)?.length === 1) {
+      this.redisClient.subscribe(subscription, this.redisCallbackHandler);
+    }
+  }
+
+  private redisCallbackHandler(message: string, channel: string) {
+    const parsedMessage = JSON.parse(message);
+    this.reverseSubscriptions
+      .get(channel)
+      ?.forEach((s) =>
+        UserManager.getInstance().getUser(s)?.emit(parsedMessage)
+      );
+  }
+
+  public unsubscribe(userId: string, subscription: string) {
+    const subscriptions = this.subscriptions.get(userId);
+    if (subscriptions) {
+      this.subscriptions.set(
+        userId,
+        subscriptions.filter((s) => s !== subscription)
+      );
     }
 
+    const reverseSubscriptions = this.reverseSubscriptions.get(userId);
+    if (reverseSubscriptions) {
+      this.reverseSubscriptions.set(
+        subscription,
+        reverseSubscriptions.filter((s) => s !== userId)
+      );
+      this.reverseSubscriptions.delete(subscription);
+      this.redisClient.unsubscribe(subscription);
+    }
+
+    return;
+  }
+
+  public userLeft(userId: string) {
+    console.log("User left ", userId);
+    this.subscriptions.get(userId)?.forEach((s) => this.unsubscribe(userId, s));
+  }
+
+  getSubscriptions(userId: string) {
+    return this.subscriptions.get(userId) || [];
 }
-
-
-private redisCallbackHandler(message:string,channel:string){
-
-    const parsedMessage = JSON.parse(message)
-    this.reverseSubscriptions.get(channel)?.forEach(s=>UserManager.getInstance().getUser(s)?.emit(parsedMessage))
-}
-
-
-public unsubscribe(userId:string,subscription:string){
-
-
-
-    return 
-
 }
--- ws/src/userManager.ts
+++ ws/src/userManager.ts
@@ -1,6 +1,9 @@
+import { SubscriptionManager } from "./subscriptionManager";
+import { User } from "./user";
+import WebSocket from "ws";
 export class UserManager {
   private static instance: UserManager;
-  private users: Map<string, string> = new Map();
+  private users: Map<string, User> = new Map();
 
   private constructor() {}
 
@@ -11,14 +14,25 @@ export class UserManager {
     return this.instance;
   }
 
-  public addUser(){
-    const id  = this.getRandomId();
-    const User = new User
+  public addUser(ws: WebSocket) {
+    const id = this.getRandomId();
+    const user = new User(id, ws);
+    this.users.set(id, user);
+    this.registerOnClose(ws, id);
+    return user;
+  }
 
-}
+  private getRandomId() {
+    return (Math.random() * 100).toString(36).substring(2, 15);
+  }
 
-
-  private getRandomId(){
-    return (Math.random()*100).toString(36).substring(2,15)
+  private registerOnClose(ws: WebSocket, id: string) {
+    ws.on("close", () => {
+      this.users.delete(id);
+      SubscriptionManager.getInstance().userLeft(id);
+    });
+  }
+  public getUser(id: string) {
+    return this.users.get(id);
   }
 }
