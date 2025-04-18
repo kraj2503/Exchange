579271616f042d456cead851ad5e44024bb2340f
kraj2503 <73212876+kraj2503@users.noreply.github.com>
2025-04-18 21:27:11 +0530

    ws:subscription

--- ws/src/subscriptionManager.ts
+++ ws/src/subscriptionManager.ts
@@ -1,6 +1,45 @@
+import { createClient, RedisClientType } from "redis";
+
+export class SubscriptionManager {
+  private static instance: SubscriptionManager;
+  private subscriptions: Map<string, string[]> = new Map();
+  private reverseSubscriptions: Map<string, string[]> = new Map();
+  private redisClient: RedisClientType;
+
+  private constructor() {
+    this.redisClient = createClient();
+    this.redisClient.connect();
+  }
+
+  public static getInstance() {
+    if (!this.instance) {
+      this.instance = new SubscriptionManager();
+    }
+    return this.instance;
+  }
+public subscribe(userId:string,subscription:string){
+
+    if(this.subscriptions.get(userId)?.includes(subscription)){
+        return 
+    }
+
+    this.subscriptions.set(userId,(this.subscriptions.get(userId)||[]).concat(subscription))
+    this.reverseSubscriptions.set(userId,(this.reverseSubscriptions.get(userId)||[]).concat(subscription))
+    
+    if(this.reverseSubscriptions.get(subscription)?.length===1){
+        this.redisClient.subscribe(subscription,this.redisCallbackHandler)
+    }
+
+}
 
 
-export class SubscriptionManager{
-    private static instance: SubscriptionManager;
+private redisCallbackHandler(message:string,channel:string){
+
+    const parsedMessage = JSON.parse(message)
+    this.reverseSubscriptions.get(channel)?.forEach(s=>UserManager.getInstance().getUser(s)?.emit(parsedMessage))
+}
+
+
+
 
 }
